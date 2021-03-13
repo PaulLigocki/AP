d8e040e97d6dea35d83e75052c7cd67297e6a206
PaulLigocki <PaulLigocki>
2021-03-13 11:56:27 +1100

    testing bash

--- do.sh
+++ do.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-if cd ~/AP/; then git pull; else git clone https://github.com/PaulLigocki/AP.git AP; fi
-cd ~/AP/ && git pull && ansible-playbook local.yml --tags vars,wpa_supplicant,ufw,disable_ipv6,disable_bluetooth,udev_rules,dnsmasq,fix_script #,hostapd,interfaces
+if cd /home/pi/AP/; then git pull; else git clone https://github.com/PaulLigocki/AP.git AP; fi
+chown pi:pi /home/pi/AP
+cd /home/pi/AP/ && git pull && ansible-playbook local.yml --tags vars,wpa_supplicant,ufw,disable_ipv6,disable_bluetooth,udev_rules,dnsmasq,fix_script #,hostapd,interfaces
