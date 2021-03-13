4698202ea902d0e5027db0bfe3773d88bc740ba0
PaulLigocki <PaulLigocki>
2021-03-13 12:05:30 +1100

    testing tags

--- do.sh
+++ do.sh
@@ -4,4 +4,5 @@ if cd /home/pi/AP/; then git pull; else git clone https://github.com/PaulLigocki
 echo 'Setting owner on Repo'
 chown pi:pi /home/pi/AP
 echo 'Attempting to run playbook'
-cd /home/pi/AP/ && git pull && ansible-playbook local.yml --tags vars,wpa_supplicant,ufw,disable_ipv6,disable_bluetooth,udev_rules,dnsmasq,fix_script #,hostapd,interfaces
+cd /home/pi/AP/ && git pull && ansible-playbook local.yml --tags vars,wpa_supplicant,ufw,disable_ipv6,disable_bluetooth,udev_rules,dnsmasq,fix_script
+#,hostapd,interfaces
