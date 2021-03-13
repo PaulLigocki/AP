bad77d74c65e9c1bb3543ec42f1e10e54e5e73ab
PaulLigocki <PaulLigocki>
2021-03-13 13:20:02 +1100

    ip a at the end

--- do.sh
+++ do.sh
@@ -6,5 +6,4 @@ chown pi:pi /home/pi/AP
 echo 'Attempting to run playbook'
 cd /home/pi/AP/ && git pull && ansible-playbook local.yml --tags vars,wpa_supplicant,ufw,disable_ipv6,disable_bluetooth,udev_rules,dnsmasq,fix_script
 #,hostapd,interfaces
-
 ip a
