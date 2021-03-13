54f76ac4872da83d56c20d63d35d05f147cd1ac1
PaulLigocki <PaulLigocki>
2021-03-13 11:58:31 +1100

    testing bash

--- do.sh
+++ do.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo 'Cloning or updating AP Repo'
 if cd /home/pi/AP/; then git pull; else git clone https://github.com/PaulLigocki/AP.git AP; fi
+echo 'Setting owner on Repo'
 chown pi:pi /home/pi/AP
+echo 'Attempting to run playbook'
 cd /home/pi/AP/ && git pull && ansible-playbook local.yml --tags vars,wpa_supplicant,ufw,disable_ipv6,disable_bluetooth,udev_rules,dnsmasq,fix_script #,hostapd,interfaces
